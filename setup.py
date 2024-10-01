ec9a7acd73135c524c9806f54a1216a86df5e369
Abdullah1Alnami <bdallhsydalnmy@gmail.com>
2024-10-01 21:43:51 +0300

    last updates

--- setup.py
+++ setup.py
@@ -4,18 +4,15 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="micrograd",
+    name="Auto Differentiation",
     version="0.1.0",
-    author="Andrej Karpathy",
-    author_email="andrej.karpathy@gmail.com",
-    description="A tiny scalar-valued autograd engine with a small PyTorch-like neural network library on top.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/karpathy/micrograd",
+    author="Abdullah Alnami",
+    author_email="bdallhsydalnmy@gmail.com",
+    description="A tiny scalar-valued auto differentiation engine with a small PyTorch-like neural network library on top.",
+    url="https://github.com/Abdullah1Allnami/Auto_Diffrentiation",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
